264c0c271991bfa2c6617d77ce2d117b91a83fbd
Dylan <dylan.hilburn@outlook.com>
2025-05-10 10:59:26 -0500

    Chapter 3 directory

---
+++ Chapter3/Conditional_Statements.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+
+int main() {
+
+    int studentGrade1{};
+    std::cout << "Enter a grade: ";
+    std::cin >> studentGrade;
+
+    // if statement
+    if (studentGrade < 70) {
+        std::cout << "You failed";
+    }
+
+    int
+
+    // if else (Unnessary Example
+    if (studentGrade < 70) {
+        std::cout << "You failed";
+    } else {
+        std::cout << "You passed";
+    }
+
+    // switch statement
+
+
+    return 0;
+}
